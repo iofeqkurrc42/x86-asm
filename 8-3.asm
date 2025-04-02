0e3cd1c34dea01813687ced02952b504cb100340
0x26e7d <geaarwhwu@hotmail.com>
2025-04-02 17:34:27 +0800

    mdf: 8-3 stack

--- 8-3.asm
+++ 8-3.asm
@@ -11,3 +11,34 @@ mov dx,[bx]
 pop ax
 pop bx
 pop ds
+
+;; 将这26字节的数据在原地反向排列
+;; 使用栈
+string db 'abcdefghijklmnopqrstuvwxyz'
+    mov cx,26
+    mov bx,string
+lppush:
+    mov al,[bx]
+    push ax
+    inc bx
+    loop lppush
+lppop:
+    pop ax
+    mov [bx],al
+    inc bx
+    loop lppop
+
+;; 使用基址变址寻址
+    mov bx,string
+    mov si,0
+    mov di,25
+order:
+    mov ah,[bx+si]
+    mov al,[bx+di]
+    mov [bx+si],al
+    mov [bx+di],ah
+    inc si
+    dec di
+    cmp si,di
+    jl order
+
